16f546340d646eaaff7979d6050c2ec759b9d30d
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-07-22 11:20:30 +0700

    icon

--- conf.lua
+++ conf.lua
@@ -1,9 +1,15 @@
 -- conf.lua
 function love.conf(t)
     t.window.title = "SlimSurvivor"
+    t.window.icon = "assets/icon/SlimeIcon.png"
     t.window.width = 800        -- ความกว้างหน้าต่าง (ไม่จำเป็นในโหมด fullscreen)
     t.window.height = 600       -- ความสูงหน้าต่าง (ไม่จำเป็นในโหมด fullscreen)
     t.window.fullscreen = false  -- เปิดโหมด fullscreen
+
+
+    -- 
     t.window.vsync = 0         -- เปิดการใช้ vertical sync (เพื่อป้องกันการฉีกของภาพ)
+    t.window.msaa = 16
+
     t.window.resizable = true  -- ไม่ให้สามารถเปลี่ยนขนาดหน้าต่างได้ (สามารถปรับได้ตามต้องการ)
 end
