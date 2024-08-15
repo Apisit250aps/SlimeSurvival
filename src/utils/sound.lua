945767f848dff89ace5404f55c852378bca35ab9
Lynnn01 <jetsadathegame@gmail.com>
2024-08-15 23:05:09 +0700

    sound

--- src/utils/sound.lua
+++ src/utils/sound.lua
@@ -4,7 +4,7 @@ SoundsPlay.__index = SoundsPlay
 function SoundsPlay:new()
     local self = setmetatable({}, SoundsPlay)
     self.src = {
-        coinSound = love.audio.newSource("assets/sounds/coin_collect.mp3", "static"),
+        coinSound = love.audio.newSource("assets/sounds/pudding.mp3", "static"),
         gameStart = love.audio.newSource("assets/sounds/game-start.mp3", "static"),
         slimeWalk = love.audio.newSource("assets/sounds/slimewalk.mp3", "static"),
         gameOver = love.audio.newSource("assets/sounds/game_over.mp3", "static")
