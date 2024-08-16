6c94af886b2fcffc3436eb8d7765834ddd086dbc
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-08-16 16:55:37 +0700

    update

--- src/entities/enemy.lua
+++ src/entities/enemy.lua
@@ -15,7 +15,7 @@ function Enemy:new(x, y, target)
     }
     self.speed = {
         min = 50,
-        base = 60,
+        base = love.math.random(50, 75), -- Set a random speed at the start
         max = 75
     }
     self.target = target
@@ -48,18 +48,43 @@ end
 
 -- Update function to move the enemy towards the target position
 function Enemy:update(dt)
-    self.speed.base = love.math.random(self.speed.min, self.speed.max)
+    -- Update speed every 2 seconds instead of every frame
+    if not self.lastSpeedUpdate or (love.timer.getTime() - self.lastSpeedUpdate) > 2 then
+        self.speed.base = love.math.random(self.speed.min, self.speed.max)
+        self.lastSpeedUpdate = love.timer.getTime()
+    end
+
+    -- Determine movement direction
+    local directionX = 0
+    local directionY = 0
+
     -- Move in the X direction
     if self.target.position.x > self.position.x then
-        self.position.x = self.position.x + self.speed.base * dt
-    else
-        self.position.x = self.position.x - self.speed.base * dt
+        directionX = 1
+    elseif self.target.position.x < self.position.x then
+        directionX = -1
     end
+
     -- Move in the Y direction
     if self.target.position.y > self.position.y then
-        self.position.y = self.position.y + self.speed.base * dt
-    else
-        self.position.y = self.position.y - self.speed.base * dt
+        directionY = 1
+    elseif self.target.position.y < self.position.y then
+        directionY = -1
+    end
+
+    -- Update position
+    self.position.x = self.position.x + directionX * self.speed.base * dt
+    self.position.y = self.position.y + directionY * self.speed.base * dt
+
+    -- Update animation based on movement direction
+    if directionX > 0 then
+        self.sprite.currentAnimation = self.animations.right
+    elseif directionX < 0 then
+        self.sprite.currentAnimation = self.animations.left
+    elseif directionY > 0 then
+        self.sprite.currentAnimation = self.animations.down
+    elseif directionY < 0 then
+        self.sprite.currentAnimation = self.animations.up
     end
 
     self.sprite.currentAnimation:update(dt)
@@ -67,9 +92,7 @@ end
 
 -- Draw function to render the enemy sprite
 function Enemy:draw()
-    -- Draw sprite
-    self.sprite.currentAnimation:draw(self.sprite.sheet, self.position.x, self.position.y, 0, self.sprite.scale,
-        self.sprite.scale)
+    self.sprite.currentAnimation:draw(self.sprite.sheet, self.position.x, self.position.y, 0, self.sprite.scale, self.sprite.scale)
 end
 
 return Enemy
