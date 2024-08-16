eea293388a50b2f1045c4f8b390257175df83afe
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-08-16 17:00:14 +0700

    patch 1.0.0

--- conf.lua
+++ conf.lua
@@ -2,11 +2,10 @@
 function love.conf(t)
     t.window.title = "SlimSurvivor"
     t.window.icon = "assets/icon/SlimeIcon.png"
-    t.window.width = 800      -- ความกว้างหน้าต่าง (ไม่จำเป็นในโหมด fullscreen)
-    t.window.height = 600     -- ความสูงหน้าต่าง (ไม่จำเป็นในโหมด fullscreen)
-    t.window.fullscreen = true -- เปิดโหมด fullscreen
-
-
+    t.window.width = 1280      -- ความกว้างหน้าต่าง (ไม่จำเป็นในโหมด fullscreen)
+    t.window.height = 800     -- ความสูงหน้าต่าง (ไม่จำเป็นในโหมด fullscreen)
+    t.window.fullscreen = false -- เปิดโหมด fullscreen
+    t.window.fullscreentype = "desktop"
     --
     t.window.vsync = 0 -- เปิดการใช้ vertical sync (เพื่อป้องกันการฉีกของภาพ)
     t.window.msaa = 16
